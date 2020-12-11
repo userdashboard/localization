85aabbd6ade096ef9d007525e7b4713ddeebbc24
benlowry <benlowry@outlook.com>
2020-12-10 16:29:11 -0800

    Remove log

--- localize.js
+++ localize.js
@@ -2,20 +2,16 @@
   const createTextManifest = require('./create-text-manifest.js')
   const cleanTranslationCache = require('./clean-translation-cache.js')
   const translateText = require('./translate-text.js')
-  console.log('translating', process.argv[2])
-  console.log('extracting text')
   const original = await createTextManifest()
   let translation
   while (!translation) {
     try {
-      console.log('translating text')
       translation = await translateText(original)
     } catch (error) {
       console.log('an error ocurred', error)
     }
   }
   try {
-    console.log('cleaning cache')
     await cleanTranslationCache(original, translation)
   } catch (error) {
     console.log('error cleaning cache', error)
